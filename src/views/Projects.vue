8e066c92c5c12bd35b6910a950491501a256d61b
Innocent <lindokuhlenkamela@gmail.com>
2022-02-12 15:07:19 +0200

    testimonials needs update

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,26 +1,21 @@
 <template>
 <h1>Projects</h1>
 <div class="card-container">
-<div class="row row-cols-1 row-cols-md-4 g-2">
-    <div class="card"  v-for="project of projects" :key="project.id" > 
-  <div class="col">
-    <div class="card h-100">
-      <img :src="project.img" class="card-img-top" alt="project.img_alt">
-  <div class="card-body">
-    <h5 class="card-title">{{project.title}}</h5>
-    <p class="card-text">{{project.desc}}</p>
-    <p class="card-text">{{project.stack}}</p>
-      <button class="button type3">
-    View Site
-
-  </button>
-    <button class="button type1">
-    View Github
-  </button>
-  </div>
+  <div class="row row-cols-1 row-cols-md-4 g-2">
+      <div class="card"  v-for="project of projects" :key="project.id" > 
+        <div class="col">
+          <div class="card h-100">
+                <img :src="project.img" class="card-img-top" alt="project.img_alt">
+              <div class="card-body">
+                <h5 class="card-title">{{project.title}}</h5>
+                <p class="card-text">{{project.desc}}</p>
+                <p class="card-text">{{project.stack}}</p>
+                <button class="button type3">View Site</button>
+                <button class="button type1">View Github</button>
+              </div>
+          </div>
+        </div>
     </div>
-  </div>
-  </div>
   </div>
 </div>
 
--- src/views/Testimonials.vue
+++ src/views/Testimonials.vue
@@ -1,55 +1,54 @@
 <template>
-<!-- test -->
- <div class="card-group">
-  <div class="card">
-    <img src="../assets/images/alex.jpg" class="card-img-top" alt="alex-image" id="img-card">
-    <div class="card-body">
-      <h5 class="card-title" id="title-h">Alex Sexwale</h5>
-      <p class="card-text" id="text-p">
-        "Lindokuhle Nkamela is dedicated like tackling 
-        all challenges presented to him, He has a bright future ahead 
-        of him." </p>
-    </div>
-  </div>
-  <div class="card">
-    <img src="../assets/images/lilita.jpg" class="card-img-top" alt="..." id="img-card">
-    <div class="card-body">
-      <h5 class="card-title" id="title-h">Lilita Ngele</h5>
-      <p class="card-text" id="text-p">hmmm.. well Lindokuhle is a helpful guy and
-         extremely gentle and understanding. 
-         always willing to learn.</p>
-    </div>
-  </div>
-  <div class="card">
-    <img src="../assets/images/Aphiwe.jpg" class="card-img-top" alt="..." id="img-card">
-    <div class="card-body">
-      <h5 class="card-title" id="title-h">Aphiwe Mpeto</h5>
-      <p class="card-text" id="text-p">hmmm.. well Lindokuhle is a helpful guy and extremely gentle and understanding. 
-                Always willing to learn.</p>
-    </div>
-  </div>
-  <div class="card">
-    <img src="../assets/images/unathi.jpg" class="card-img-top" alt="..." id="img-card">
-    <div class="card-body">
-      <h5 class="card-title" id="title-h">Unathi Qolweni</h5>
-      <p class="card-text" id="text-p">Lindokuhle pays attention to improve the aesthetics of the site as well as the 
-                functionality. Throughout the entire process he is responsive, 
-                and willing to work through issues as they arise. 
-      </p>
-    </div>
-  </div>
-  <div class="card">
-    <img src="../assets/images/Sindi.jpg" class="card-img-top" alt="..." id="img-card">
-    <div class="card-body">
-      <h5 class="card-title" id="title-h">Sindile Makhathini</h5>
-      <p class="card-text" id="text-p">I met Lindokuhle at the Academy and I have observed that he is a quiet 
-                and hardworking individual. He is a goal-orientated person who 
-                strives to be the best that he can be.
-      </p>
+<h1>Testimonials</h1>
+<div class="card-container">
+  <div class="row row-cols-1 row-cols-md-4 g-2">
+      <div class="card"  v-for="testimonial of testimonials" :key="testimonial.id" > 
+        <div class="col">
+          <div class="card h-100">
+                <img :src="testimonial.img" class="card-img-top" alt="project.img_alt">
+              <div class="card-body">
+                <h5 class="card-title">{{testimonial.title}}</h5>
+                <p class="card-text">{{testimonial.desc}}</p>
+              </div>
+          </div>
+        </div>
     </div>
   </div>
 </div>
 </template>
+<script>
+export default {
+    data() {
+        return {
+            testimonials:[]
+        }
+    },
+
+
+    mounted(){
+        fetch('https://nodejsexpressapilindo.herokuapp.com/testimonials')
+        .then(res => res.json())
+        .then(data => {this.projects = data})
+        .catch(err => console.log(err.message))
+    }
+}
+</script>
+
+<style scoped>
+    .project h2{
+        background: white;
+        padding: 20px;
+        border-radius: 10px;
+        margin: 10px auto;
+        max-width: 600px;
+        cursor: pointer;
+        color: #444;
+    }
+    img{
+      height: 300px;
+      width:300px ;
+    }
+    </style>
 <style scoped>
 #text-p,#title-h{
 color: black;
